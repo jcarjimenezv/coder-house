4155e096e27aa3ab2941c60454f18ab5c39bebe4
“JuanKJimenez” <ext_jcarjimenezv@falabella.cl>
2022-11-06 15:25:55 -0500

    examples implementation for class number 1

---
+++ clase_1/desafioClase1.js
@@ -0,0 +1,32 @@
+const firstName = "Juan"
+console.log("Name: " + firstName)
+console.log("")
+let age = 34
+console.log("Age: " + age)
+console.log("")
+const price = 99.90
+console.log("Price: " + age)
+console.log("")
+const favTVShowsList = ["Dark", "Mr Robot", "Castlevania"] 
+console.log("favTVShowsList: ", favTVShowsList)
+console.log("")
+const favMovies = [{
+    name: "Suicide Squad",
+    characters: ["Will Smith", "Margot Robbie", "Jared Leto"],
+    age: 2016
+},{
+    name: "Titanic",
+    characters: ["Leonardo Di Caprio", "Kate Winslet"],
+    age: 1997
+}]
+
+console.log("favMovies: ", favMovies)
+console.log("")
+
+age += 1
+console.log("age +1 : ", age)
+console.log("")
+
+favTVShowsList.push("Stranger Things")
+console.log("favTVShowsList + Stranger Things: ", favTVShowsList)
+
