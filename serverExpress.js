d90e4c91098755b3a1eafdc9d586ec0915779965
“JuanKJimenez” <ext_jcarjimenezv@falabella.cl>
2022-11-28 20:40:27 -0500

    class 7 express

--- serverExpress.js
+++ serverExpress.js
@@ -1,12 +1,39 @@
 const express = require("express");
 
 const app = express();
+const frase = "Hola mundo cómo están";
 
-app.get("/api/print", (req, res) => {
-  res.send('<h1 style="color: blue">Bienvenidos al servidor express</h1>');
+app.get("/api/frase", (req, res) => {
+  res.status(200).send({ frase: frase });
 });
 
-const PORT = 8080;
+app.get("/api/letras/:num", (req, res) => {
+  const number = parseInt(req.params.num);
+  if (isNaN(number)) {
+    res.status(400).send({ error: "El parametro no es un numero" });
+  }
+
+  if (frase.length < number) {
+    res.status(400).send({ error: "Fuera de rango" });
+  }
+
+  res.status(200).send({ letra: frase.charAt(number - 1) });
+});
+
+app.get("/api/palabras/:num", (req, res) => {
+  const number = parseInt(req.params.num);
+  if (isNaN(number)) {
+    return res.status(400).send({ error: "El parametro no es un numero" });
+  }
+
+  if (frase.split(" ").length < number) {
+    return res.status(400).send({ error: "Fuera de rango" });
+  }
+
+  return res.status(200).send({ palabra: frase.split(" ")[number] });
+});
+
+const PORT = 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Listening app port ${server.address().port}`);
